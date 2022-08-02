3cbbe3757fa60853349f4dc7670fd071fdb52503
Mateus Pincho <mateuspoliveira.ee@gmail.com>
2022-08-02 09:21:18 -0300

    Adicionando as versões atualizadas do projeto

--- Pedido2.0.h
+++ Pedido2.0.h
@@ -11,18 +11,18 @@ private:
 
 public:
      Pedido();
-     void adicionar_pedido_comprimido(string , Farmacia );       // adiciona um pedido procurando por ele na lista de comprimidos
-     void adicionar_pedido_liquido(string, Farmacia );           // adiciona um pedido procurando por ele na lista de liquidos
+     void adicionar_pedido_comprimido(string,int, Farmacia );       // adiciona um pedido procurando por ele na lista de comprimidos
+     void adicionar_pedido_liquido(string,int, Farmacia );           // adiciona um pedido procurando por ele na lista de liquidos
      void remover_pedido_comprimido(string);                     // remove um remedio da lista de comprimidos
      void remover_pedido_liquido(string);                        // remove um remedio da lista de liquidos
      void exibir_pedido_comprimido();
      void exibir_pedido_liquido();
      void concluir_pedido();
      void cancelar_pedido();
-     void verifica_tarja(string );
+     bool verifica_tarja(string);
      void verifica_desconto();
-     bool ordenar_preco();
-
+     static bool ordenar_preco_comprimido(Comprimido, Comprimido);
+     static bool ordenar_preco_liquido(Liquido, Liquido);
 };
 
 
